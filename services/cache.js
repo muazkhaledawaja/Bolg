31df842e1b658852e3e470f299aa1e33e4065ff8
Moath Awaja <31365698+muazkhaledawaja@users.noreply.github.com>
2023-07-07 05:00:59 +0300

    create cache layer

---
+++ services/cache.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+
+
+const exec = mongoose.Query.prototype.exec; // save a reference to the original exec function
+
+mongoose.Query.prototype.exec = async function () {
+
+    const key = JSON.stringify(
+        Object.assign({}, this.getQuery(),
+            { collection: this.mongooseCollection.name, })
+    )
+
+    // See if we have a value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    // If we do, return that
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
+        return Array.isArray(doc) ?
+            doc.map(d => new this.model(d)) :
+            new this.model(doc);
+
+
+    }
+    // Otherwise, issue the query and store the result in redis
+    const result = await exec.apply(this, arguments); // call the original exec function
+
+    client.set(key, JSON.stringify(result));
+
+    return result;
+}
